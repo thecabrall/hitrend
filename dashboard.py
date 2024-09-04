a5ebe89ea805cc54bf07ca91ac1d1bffec89c23f
thecabrall <gruposppider@gmail.com>
2024-09-04 13:56:49 -0300

    v1.03

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 
 with st.sidebar:
-    st.page_link("dashboard.py", label="Início", icon="🔅")
+    st.page_link("dashboard.py", label="Início", icon="🔅",disabled=True)
     st.page_link("pages/search.py", label="Procurar Item", icon="🔎")
     st.page_link("pages/sandbox.py", label="Sandbox", icon="🛠️")
